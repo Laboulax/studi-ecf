16f73a3cb99ad78fadc32cddae4226354c453c7f
Laboulax <antoinelaboulais@yahoo.fr>
2025-07-21 21:10:51 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-ites<?php
-/*
+<?php
+
 require_once 'code/core/Router.php';
 require_once 'code/controllers/HomeController.php';
 require_once 'code/controllers/AuthController.php';
@@ -9,7 +9,7 @@ require_once 'code/controllers/ProfileController.php';
 $router = new Router();
 
 // Définir les routes
-$router->addRoute('/ECF_mvc/index.php?home', new HomeController());
+$router->addRoute('', new HomeController());
 $router->addRoute('/ECF_mvc/index.php?login', new AuthController('login'));
 $router->addRoute('/ECF_mvc/index.php?register', new AuthController('register'));
 $router->addRoute('/ECF_mvc/index.php?search', new SearchController());
@@ -17,6 +17,6 @@ $router->addRoute('/ECF_mvc/index.php?profile', new ProfileController());
 
 // Gérer la requête
 $uri = $_SERVER['REQUEST_URI'];
-$router->handleRequest($uri);*/
+$router->handleRequest($uri);
 
-echo ("mange mes petites couilles de raisins secs !!!!!!!!!!!!!!!!!!! sale fdp");
+
