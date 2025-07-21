25788df332fb9eed6d3a2806a80ca42996331f89
Laboulax <antoinelaboulais@yahoo.fr>
2025-07-21 21:21:16 +0200

    Update HomeController.php

--- code/controllers/HomeController.php
+++ code/controllers/HomeController.php
@@ -2,8 +2,8 @@
 
 class HomeController {
     public function handle() {
-        require_once 'code/views/header.php';
-        require_once 'code/views/home.php';
-        require_once 'code/views/footer.php';
+        require_once '../code/views/header.php';
+        require_once '../code/views/home.php';
+        require_once '../code/views/footer.php';
     }
 }
