9fa15d4e6cf4b63b300adeb4f61a808d55e16d5b
Laboulax <antoinelaboulais@yahoo.fr>
2025-07-22 12:00:55 +0200

    Update search.php

--- code/views/search.php
+++ code/views/search.php
@@ -64,7 +64,8 @@ require_once 'code/config/config.php';
 
 
             try {
-                $bdd = new PDO("mysql:host=$servername;dbname= $db_name", $username, $password);
+                
+                    $bdd  = new PDO("mysql:host=" . $servername . ";dbname=" .  $db_name, $username, $password);
                 $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOException $e) {
                 echo "Erreur : " . $e->getMessage();
